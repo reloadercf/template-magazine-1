62ec145a0cc451d358c2185714744337e136f849
Eduardo Benitez <eduisic94@gmail.com>
2019-10-28 12:09:28 -0600

    organizando componentes

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React,  { Component } from 'react';
 import store from './store/store'
 import {Provider} from 'react-redux'
-import Rutas from './components/Rutas'
+import Rutas from './src/screens/Rutas'
 
 
 
